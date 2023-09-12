4f3120bb0749cfbc11f1e128507b06246f68ad4a
Md. Zaber Al Hamid <86970972+mdzaberalhamid@users.noreply.github.com>
2023-09-12 21:29:29 +0600

    Update app.js

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,1 +1,41 @@
 // alert("Hi, this is a test!");
+
+// set initial count
+let count = 0;
+
+// select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    // console.log(btn);
+
+    btn.addEventListener('click', function(e){
+        // console.log(e.currentTarget.classList);
+        
+        const classes = e.currentTarget.classList;
+        
+        if (classes.contains('decrease')) {
+            count--;
+            // console.log(count);
+        }
+        else if (classes.contains('increase')) {
+            count++;
+        }
+        else {
+            count = 0;
+        }
+
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        else if (count < 0) {
+            value.style.color = "red";
+        }
+        else if (count == 0) {
+            value.style.color = "#222";
+        }
+
+        value.textContent = count;
+    });
+});
